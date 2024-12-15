5605e11532c4987d4f7d1d9d5e29348f6ad5ada5
ethan jiang <ethan1.jiang@gmail.com>
2024-12-15 15:29:13 +0800

    update

---
+++ exam_pydantic/pydantic_schema.py
@@ -0,0 +1,46 @@
+import json
+from enum import Enum
+
+from typing import Annotated
+import rich
+
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+
+
+class FooBar(BaseModel):
+    count: int
+    size: float | None = None
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
+    not_given = 'not_given'
+
+
+class MainModel(BaseModel):
+    """
+    This is the description of the main model
+    """
+
+    model_config = ConfigDict(title='Main')
+
+    foo_bar: FooBar
+    gender: Annotated[Gender | None, Field(alias='Gender')] = None
+    snap: int = Field(
+        default=42,
+        title='The Snap',
+        description='this is the value of snap',
+        gt=30,
+        lt=50,
+    )
+
+
+main_model_schema = MainModel.model_json_schema()  # (1)!
+rich.print(main_model_schema)
+
+print()
+schema_dump2 = json.dumps(main_model_schema, indent=2)  #  (2)!
+rich.print(schema_dump2)
